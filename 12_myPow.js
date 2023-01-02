561a2b17aae1332a46ae4754d79afb0f9537ad25
Thomas K <thmskffr@gmail.com>
2023-01-02 16:45:55 +0100

    12 done

--- 12_myPow.js
+++ 12_myPow.js
@@ -1,15 +1,22 @@
 // exponents are all integers
 function main(num, pow) {
+  let result = num;
   if (pow > 1) {
     for (let i = 1; i < pow; i++) {
-      num *= num;
+      result *= num;
     }
-    return num;
-  } else if (pow < 0) {
-    for(let i = 0; i > pow; i--) {
-    num /= num;
+    return result;
+  } else if (pow < -1) {
+    for (let i = -1; i > pow; i--) {
+      result *= num;
     }
-    return num;
-  } else if()
+    return 1 / result;
+  } else if (pow === 1) {
+    return result;
+  } else if (pow === -1) {
+    return 1 / result;
+  }
+  return 1;
 }
+console.log("test : ", main(3, 1));
 module.exports = main;
