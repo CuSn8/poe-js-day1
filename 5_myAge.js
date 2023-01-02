2e786b9b9faf8a7569b358cbc842bb67df4694f0
Thomas K <thmskffr@gmail.com>
2023-01-02 15:34:35 +0100

    5 and 6 done

---
+++ 5_myAge.js
@@ -0,0 +1,7 @@
+function main(age) {
+  if (age >= 18) {
+    return "Majeur";
+  }
+  return "Mineur";
+}
+module.exports = main;
---
+++ 6_myEvilAge.js
@@ -0,0 +1,7 @@
+function main(evilAge) {
+    if(evilAge >= 18) {
+        return "Majeur";
+    }
+    return "Mineur";
+}
+module.exports = main;
