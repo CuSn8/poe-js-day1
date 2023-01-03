d6404f0291a6d8443508a5070083eca4f2eb6449
Thomas K <thmskffr@gmail.com>
2023-01-03 10:07:26 +0100

    firTree.js level 1 done

---
+++ firTree.js
@@ -0,0 +1,19 @@
+function main(rows) {
+  let stars = [];
+  for (let i = 1; i <= rows; i++) {
+    let row = [];
+    for (let k = rows - i; k > 0; k--) {
+      row.push(" ");
+    }
+    for (let j = 1; j <= i; j++) {
+      row.push("*");
+      row.push(" ");
+    }
+    stars.push(row);
+  }
+  for (let i = 0; i < rows; i++) {
+    console.log(stars[i].join(""));
+  }
+}
+main(3);
+module.exports = main;
