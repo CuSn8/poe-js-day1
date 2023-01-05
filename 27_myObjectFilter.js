ca766eee0c84b9d5710acd485a51ce99e62dd730
Thomas K <thmskffr@gmail.com>
2023-01-05 11:42:57 +0100

    27 corrected

--- 27_myObjectFilter.js
+++ 27_myObjectFilter.js
@@ -1,7 +1,13 @@
 function myObjectFilter(userArray) {
   return userArray
     .filter((user) => user.age >= 18)
-    .map((user) => [user.surname + " " + user.name]);
+    .map((user) => user.surname + " " + user.name);
 }
+
+console.log(myObjectFilter([ 
+  {name: "A", surname: "a", age: 12}, 
+  {name: "B", surname: "b", age: 30},
+  {name: "C", surname: "c", age: 30} 
+]));
 
 module.exports = myObjectFilter;
