5f641f87765078abdf5675d2acd9a55e6068a295
Thomas K <thmskffr@gmail.com>
2023-01-11 15:29:22 +0100

    corrected 30

--- 30_myObjectValues.js
+++ 30_myObjectValues.js
@@ -1,10 +1,12 @@
 function myObjectValues(object) {
   const entries = Object.entries(object);
   const result = entries
-    .filter((entry) => typeof entry[1] !== "function")
+    .filter((entry) => typeof entry[1] !== "function" && typeof entry[1] !== "object")
     .map((entry) => entry.join(": "))
     .join(", ");
   return result;
 }
+
+console.log(myObjectValues({name: "Ganster", surname: "Guilian", age: 24, presentation: () => {}})); // "name: Ganster, surname: Guilian, age: 24"
 
 module.exports = myObjectValues;
--- 31_myCalc.js
+++ 31_myCalc.js
@@ -1,7 +1,39 @@
 class Calc {
-    constructor();
-
-    compute(string) {
-        string.split()
+  constructor(result = 0, history = []) {
+    this.result = result;
+    this.history = history;
+  }
+  compute(string) {
+    const numbers = string.split(/[\+\-\*\/]/).map((item) => parseInt(item));
+    const operator = string.split(/[0-9]/).filter((i) => i.length > 0);
+    console.log("numbers", numbers);
+    console.log("operator", operator);
+    switch (operator[0]) {
+      case "+":
+        return `${string}=${numbers[0] + numbers[1]}`;
+        break;
+      case "-":
+        return `${string}=${numbers[0] - numbers[1]}`;
+        break;
+      case "*":
+        return `${string}=${numbers[0] * numbers[1]}`;
+        break;
+      case "/":
+        return `${string}=${numbers[0] / numbers[1]}`;
+        break;
+      default:
+        return "Invalid operation";
     }
+  }
 }
+
+let calc = new Calc();
+
+console.log(calc.compute("1+2")); //"1+2=3"
+console.log(calc.compute("2-1")); //"2-1=1"
+console.log(calc.compute("2*3")); //"2*3=6"
+console.log(calc.compute("10/2")); //"10/2=5"
+/*
+console.log(calc.lastResult()); //5
+console.log(calc.history()); //["1+2=3","2-1=1","2*3=6","10/2=5"]
+*/
